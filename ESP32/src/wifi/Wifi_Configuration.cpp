09a2de05ddb18390e3c85024479e8bd953f33982
MorasDeFresa <bencito10@gmail.com>
2025-04-25 11:37:02 -0500

    add  Wifi configuration

--- ESP32/src/wifi/Wifi_Configuration.cpp
+++ ESP32/src/wifi/Wifi_Configuration.cpp
@@ -1,7 +1,24 @@
 #include "Wifi_Configuration.h"
-
+#include "Wifi_Credentials.h"
 #include "WiFi.h"
+#include <WiFiClient.h>
 
-void WifiConnection(){
-    
+std::array<IPAddress,1> WifiConnection(){
+    std::array<IPAddress,1> ip;
+    WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+    Serial.print("Connecting to ");
+    Serial.print(WIFI_SSID);
+    Serial.print(" ");
+    while(WiFi.status()!= WL_CONNECTED){
+        delay(500);
+        Serial.print('.');
+    }
+    Serial.println('.');
+    ip[0]=WiFi.localIP();
+    Serial.println("----------------------------------");
+    Serial.println("Successful connection");
+    Serial.print("IP ESP32: ");
+    Serial.println(ip[0]);
+    Serial.println("----------------------------------");
+    return ip;
 }
---
+++ ESP32/src/wifi/Wifi_Configuration.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_CONFIGURATION_H
+#define WIFI_CONFIGURATION_H
+
+#include "Wifi_Configuration.h"
+#include "WiFi.h"
+#include <WiFiClient.h>
+
+std::array<IPAddress,1> WifiConnection();
+
+#endif
