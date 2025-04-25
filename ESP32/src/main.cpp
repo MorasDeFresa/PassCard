cbb37ce08a14d1c11ecdd9a4e5b587e0b5beb40b
MorasDeFresa <bencito10@gmail.com>
2025-04-25 11:37:47 -0500

    update Integration new modules

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,8 +1,11 @@
-#include "sensors/Manage_RC522.H"
+#include "sensors/Manage_RC522.h"
+#include "wifi/Wifi_Configuration.h"
 
 void setup(){
   Serial.begin(9600);
+  std::array<IPAddress,1> IP_Esp32= WifiConnection();
   RC522_BeginSensor();
+  
 }
 
 void loop(){
