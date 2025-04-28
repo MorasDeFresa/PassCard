9bff9bd61e1cdb5d06949e9c2cb2ad6ba92cfede
RAFAELRGR <rafaelronaldog@gmail.com>
2025-04-27 21:12:24 -0500

    update page attendace

--- platform/src/app/frontend/dashboard/page.jsx
+++ platform/src/app/frontend/dashboard/page.jsx
@@ -6,7 +6,6 @@ export default function Dashboard() {
     <div>
       <Navbar />
       <div className="p-4">
-        <h1 className="text-2xl font-semibold mb-4">Registro de Asistencia</h1>
         <AttendanceTable />
       </div>
     </div>
