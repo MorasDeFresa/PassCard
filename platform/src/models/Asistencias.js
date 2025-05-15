c1c1e94d362928392ef28fcfe5ffe48a82784ab7
MorasDeFresa <bencito10@gmail.com>
2025-05-15 09:42:30 -0500

    update Models: change name and unnecesary values

--- platform/src/models/Asistencias.js
+++ platform/src/models/Asistencias.js
@@ -1,25 +1,20 @@
 const { Sequelize, DataTypes, Model } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define(
-        'Asistencias',
-        {
-            id_asistencia: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            fecha_Asistencias: {
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
-        },
-        {
-            freezeTableName: true,
-            timestamps: true,
-            paranoid: true,
-        }
-    )
-}
-
+  sequelize.define(
+    "Asistencias",
+    {
+      id_asistencia: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+};
--- platform/src/models/Asistentes.js
+++ platform/src/models/Asistentes.js
@@ -1,33 +1,32 @@
 const { Sequelize, DataTypes, Model } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define(
-        'Asistentes',
-        {
-            id_Asistente: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            nombre_Asistente: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-            },
-            correo_Asistente: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-            },
-            token_Asistente: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-            },
-        },
-        {
-            freezeTableName: true,
-            timestamps: true,
-            paranoid: true,
-        }
-    )
-}
-
+  sequelize.define(
+    "Asistentes",
+    {
+      id_Asistente: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nombre_Asistente: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      correo_Asistente: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      token_Asistente: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+};
