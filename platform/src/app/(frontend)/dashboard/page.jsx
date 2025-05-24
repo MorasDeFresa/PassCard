1fb96e6f62970b149c5274a6f84bd855c0712555
MorasDeFresa <bencito10@gmail.com>
2025-05-24 12:53:11 -0500

    ✨feature: integration dashboard con data from postgresql

--- platform/src/app/(frontend)/dashboard/page.jsx
+++ platform/src/app/(frontend)/dashboard/page.jsx
@@ -1,12 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import AttendanceTable from "../components/attendanceTable";
 import Navbar from "../components/narvar";
 
 export default function Dashboard() {
+  const [attendanceData, setAttendanceData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/asistencias`,
+          {
+            method: "GET",
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        setAttendanceData(data);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="p-4">
-        <AttendanceTable />
+        <AttendanceTable data={attendanceData} />
       </div>
     </div>
   );
